16790c49e416d97d6de5f5975376580c4c82c9ba
George <paradox81ru@yandex.ru>
2023-04-11 07:31:20 +0300

    correct mail settings

--- DoxPortal/settings.py
+++ DoxPortal/settings.py
@@ -171,12 +171,21 @@ CACHES = {
 }
 
 # Настройки электронной почты
-EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = True
-EMAIL_HOST = 'smtp.yandex.com'
-EMAIL_PORT = '587'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = '465'
 EMAIL_HOST_USER = 'paradox81ru@yandex.ru'
-EMAIL_HOST_PASSWORD = 'cosmOS_987'
+EMAIL_HOST_PASSWORD = 'jgxvaczffgttdaba'
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = '587'
+# EMAIL_HOST_USER = 'paradox81ru@gmail.com'
+# EMAIL_HOST_PASSWORD = 'cosmOS_987'
+
 
 SERVER_EMAIL = 'paradox81ru@mail.ru'
 
